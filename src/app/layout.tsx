66edf24d048266ff6ec6abd252807b9d2625d49e
Bruna Braguin <bruna.a.braguin@gmail.com>
2025-05-25 20:24:24 -0300

    fix: suppressHydrationWarning true

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import type React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -5,6 +6,8 @@ import "./globals.css";
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
+  display: "swap", 
+  preload: true,
 });
 
 export const metadata: Metadata = {
@@ -22,8 +25,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR">
-      <body className={`${inter.variable} antialiased`}>
+    <html lang="pt-BR" className={inter.variable}>
+      <body className="antialiased" suppressHydrationWarning={true}>
         {children}
       </body>
     </html>
